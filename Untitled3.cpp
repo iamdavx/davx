be1910271d87bca795d64c84a025ee602c0fb856
iamdavx <99694842+iamdavx@users.noreply.github.com>
2022-02-18 20:06:39 -0300

    test
    test

---
+++ Untitled3.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+ struct aluno {
+ 	char nome[30], cpf[11], rg[10];
+	int idade; 
+	float altura, peso; 
+		
+ };
+ 
+ void imprimir(struct aluno aluno1){
+ 	 	printf("Nome %s", aluno.nome);
+ 		printf("CPF %s", aluno.cpf);
+ }
+ 
+int main (){
+	struct aluno aluno1; 
+	pritnf("Digite o nome:")
+	fgets(aluno.nome, 30, stdin);
+	printf("Digite o cpf: ");
+	scanf("%s", aluno.cpf)
+	
+	
+printf("Nome %s", aluno.nome)
+printf("CPF %s", aluno1.cpf)	
+imprimir(aluno1);	
+	
+
+
+ return 0; }
